0f2d376fa06a2fa22b4d76d54e2ebecdfe5eb7b8
Inix3K <jmbise@gmail.com>
2020-01-21 11:56:45 -0600

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
-      theme: ThemeData.dark(),
+      theme: ThemeData(),
       home: RandomWords(),
     );
   }
